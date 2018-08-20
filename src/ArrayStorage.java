ceb406cafbd36cc6040145cbb12b5b4715f07858
Arjun7682 <pasha4ppl@gmail.com>
2018-08-20 18:57:58 +0300

    HW2

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -7,27 +7,48 @@ public class ArrayStorage {
     private Resume[] storage = new Resume[10000];
     private int size = 0;
 
-    void clear() {
+    public void clear() {
         Arrays.fill(storage, 0, size, null);
         size = 0;
     }
 
-    void save(Resume r) {
-        //if (r == null || r.uuid == null || get(r.uuid) != null) return;
-        storage[size] = r;
+    public void update(Resume resume) {
+        if (isExist(resume.uuid)) {
+            resume.uuid = "updated";
+        }
+        else {
+            System.out.println(resume.uuid + " отсутствует в хранилище.");
+        }
+    }
+
+    public void save(Resume resume) {
+        if (size == storage.length) {
+            System.out.println("Нет места для хранения нового резюме.");
+            return;
+        }
+        if (isExist(resume.uuid)) {
+            System.out.println(resume.uuid + " уже есть в хранилище.");
+            return;
+        }
+        storage[size] = resume;
         size++;
     }
 
-    Resume get(String uuid) {
+    public Resume get(String uuid) {
         for (int i = 0; i < size; i++) {
             if (storage[i].uuid.equals(uuid)) {
                 return storage[i];
             }
         }
+        System.out.println(uuid + " отсутствует в хранилище.");
         return null;
     }
 
-    void delete(String uuid) {
+    public void delete(String uuid) {
+        if (!isExist(uuid)) {
+            System.out.println(uuid + " отсутствует в хранилище.");
+            return;
+        }
         for (int i = 0; i < size; i++) {
             if (storage[i].uuid.equals(uuid)) {
                 System.arraycopy(storage, i + 1, storage, i, size - i - 1);
@@ -40,11 +61,20 @@ public class ArrayStorage {
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    Resume[] getAll() {
+    public Resume[] getAll() {
         return Arrays.copyOfRange(storage, 0, size);
     }
 
-    int size() {
+    public int size() {
         return size;
+    }
+
+    public boolean isExist(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
