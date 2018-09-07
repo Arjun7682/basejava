905a03014d53f93560bc39f29fe65275c07491e7
Arjun7682 <pasha4ppl@gmail.com>
2018-09-07 17:00:03 +0300

    AbstractFileStorage refactored, AbstractStorage added logger

--- src/ru/javawebinar/basejava/MainFile.java
+++ src/ru/javawebinar/basejava/MainFile.java
@@ -1,28 +1,21 @@
 package ru.javawebinar.basejava;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainFile {
     public static void main(String[] args) {
-        File dir = new File("C:\\Users\\pasha\\YandexDisk\\Git\\basejava\\src\\ru\\javawebinar\\basejava");
-
-        for (File file : getFileList(dir)) {
-            System.out.println(file.getName());
-        }
+        File dir = new File("C:\\Users\\pasha\\YandexDisk\\Git\\basejava");
+        getFileList(dir, "");
     }
 
-    public static List<File> getFileList(File dir) {
-        List<File> files = new ArrayList<>();
-
+    public static void getFileList(File dir, String offset) {
         for (File file : dir.listFiles()) {
             if (file.isDirectory()) {
-                files.addAll(getFileList(file));
+                System.out.println("Dir: " + offset + file.getName());
+                getFileList(file, "  ");
             } else {
-                files.add(file);
+                System.out.println(offset + file.getName());
             }
         }
-        return files;
     }
 }
