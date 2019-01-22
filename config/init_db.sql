6f09894781a30c0d74e385b9c2b2892a332e6c93
Arjun7682 <pasha4ppl@gmail.com>
2019-01-22 18:42:10 +0300

    HW15_UP2: init_db updated

--- config/init_db.sql
+++ config/init_db.sql
@@ -14,3 +14,22 @@ CREATE TABLE public.contact
 
 create unique index contact_uuid_type_index
   on public.contact (resume_uuid, type);
+
+create table section
+(
+  id          serial      not null
+    constraint table_name_pkey
+    primary key,
+  type        text        not null,
+  value       text        not null,
+  resume_uuid varchar(36) not null
+    constraint table_name_resume_uuid_fk
+    references resume
+    on delete cascade
+);
+
+alter table section
+  owner to postgres;
+
+create unique index table_name_id_uindex
+  on section (id);
