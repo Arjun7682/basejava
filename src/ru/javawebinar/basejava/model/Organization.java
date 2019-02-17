827c17988636ac51184a06d58a7ef8c8544ec46a
Arjun7682 <pasha4ppl@gmail.com>
2019-02-17 18:19:36 +0300

    HW16_up2: refactored organization; add index.html; fix bug.

--- src/ru/javawebinar/basejava/model/Organization.java
+++ src/ru/javawebinar/basejava/model/Organization.java
@@ -40,16 +40,8 @@ public class Organization implements Serializable {
         return orgEntries;
     }
 
-    public void setOrgEntries(List<Position> orgEntries) {
-        this.orgEntries = orgEntries;
-    }
-
-    public Link getCompany() {
+     public Link getCompany() {
         return company;
-    }
-
-    public void setCompany(Link company) {
-        this.company = company;
     }
 
     @Override
@@ -66,38 +58,6 @@ public class Organization implements Serializable {
         return Objects.hash(orgEntries, company);
     }
 
-    public void addPosition(LocalDate begin, LocalDate end, String position, String description) {
-        if (getOrgEntryIndex(position) == -1) {
-            orgEntries.add(new Position(begin, end, position, description));
-        }
-    }
-
-    private int getOrgEntryIndex(String position) {
-        for (int i = 0; i < orgEntries.size(); i++) {
-            if (orgEntries.get(i).getTitle().equals(position)) {
-                return i;
-            }
-        }
-        return -1;
-    }
-
-    public void removePosition(String position) {
-        int orgEntryIndex = getOrgEntryIndex(position);
-        if (orgEntryIndex != -1) {
-            orgEntries.remove(orgEntryIndex);
-        }
-    }
-
-    public void updatePosition(LocalDate begin, LocalDate end, String position, String description) {
-        int orgEntryIndex = getOrgEntryIndex(position);
-        if (orgEntryIndex != -1) {
-            Position entry = orgEntries.get(orgEntryIndex);
-            entry.setStartDate(begin);
-            entry.setEndDate(end);
-            entry.setDescription(description);
-        }
-    }
-
     @XmlAccessorType(XmlAccessType.FIELD)
     public static class Position implements Serializable {
         private static final long serialVersionUID = 1L;
@@ -110,14 +70,6 @@ public class Organization implements Serializable {
         private String description;
 
         public Position() {
-        }
-
-        public Position(int startYear, Month startMonth, String title, String description) {
-            this(of(startYear, startMonth), NOW, title, description);
-        }
-
-        public Position(int startYear, Month startMonth, int endYear, Month endMonth, String title, String description) {
-            this(of(startYear, startMonth), of(endYear, endMonth), title, description);
         }
 
         public Position(LocalDate startDate, String title, String description) {
@@ -138,32 +90,16 @@ public class Organization implements Serializable {
             return startDate;
         }
 
-        public void setStartDate(LocalDate startDate) {
-            this.startDate = startDate;
-        }
-
         public LocalDate getEndDate() {
             return endDate;
-        }
-
-        public void setEndDate(LocalDate endDate) {
-            this.endDate = endDate;
         }
 
         public String getTitle() {
             return title;
         }
 
-        public void setTitle(String title) {
-            this.title = title;
-        }
-
         public String getDescription() {
             return description;
-        }
-
-        public void setDescription(String description) {
-            this.description = description;
         }
 
         @Override
